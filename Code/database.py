4a3e1372611bc16e47e423688a320af3901444bf
Domenico Manna <mannadomenico2849@gmail.com>
2019-09-23 12:39:49 -0400

    Move files

---
+++ Code/database.py
@@ -0,0 +1,25 @@
+from pymongo import MongoClient 
+
+connectionUrl = "mongodb+srv://Dom:password1234@cluster0-tgp6l.gcp.mongodb.net/test?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE"
+
+client = MongoClient(connectionUrl)
+
+db = client.get_database("Property_Management")
+
+def getAllTenants():
+    tenantCollection = db['Tenant']
+    return list(tenantCollection.find())
+
+def getAllLandlords():
+    landlordCollection = db['LandLord']
+    return list(landlordCollection.find())
+
+def getTenant(tenantUsername):
+    tenantCollection = db['Tenant']
+    return tenantCollection.find_one({'Username': tenantUsername})
+
+def getLandLord(landLordUsername):
+    landLordCollection = db['LandLord']
+    return landLordCollection.find_one({'Username': landLordUsername})
+
+
