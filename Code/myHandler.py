3d80b873c7ef6f5e9511444ad6b79c189866078c
Noble Mathew <nmeastern@gmail.com>
2019-09-24 20:41:33 -0400

    register page

--- Code/myHandler.py
+++ Code/myHandler.py
@@ -1,5 +1,5 @@
 import database as db
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 @app.route('/')
@@ -26,4 +26,13 @@ def showLandlordProfile():
     return render_template('landLord.html', landLord = landLord)
 
 
-
+# Route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
