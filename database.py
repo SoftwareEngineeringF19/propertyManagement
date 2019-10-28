7ce81fcd7ae74fa31b14093530d787374595ccf3
Domenico Manna <mannadomenico2849@gmail.com>
2019-10-28 00:43:55 -0400

    add ability to change avatar

---
+++ database.py
@@ -0,0 +1,32 @@
+import pymongo, config
+from fileHandler import FileHandler
+
+
+connectionUrl = "mongodb+srv://Dom:password1234@cluster0-tgp6l.gcp.mongodb.net/test?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE"
+
+client = pymongo.MongoClient(connectionUrl)
+
+db = client.get_database("Property_Management")
+fileHandler = FileHandler()
+
+def getAllTenants():
+    tenantCollection = db['Tenant']
+    return list(tenantCollection.find())
+
+def getAllLandlords():
+    landlordCollection = db['LandLord']
+    return list(landlordCollection.find())
+
+def getTenant(tenantUsername):
+    tenantCollection = db['Tenant']
+    tenant = tenantCollection.find_one({'Username': tenantUsername})
+    profileImage = fileHandler.getImagePathOrDefault(config.avatarsFolder, tenantUsername)
+    tenant['profileImage'] = profileImage
+    return tenant
+
+def getLandLord(landLordUsername):
+    landLordCollection = db['LandLord']
+    landLord = landLordCollection.find_one({'Username': landLordUsername})
+    profileImage = fileHandler.getImagePathOrDefault(config.avatarsFolder, landLordUsername)
+    landLord['profileImage'] = profileImage
+    return landLord
---
+++ fileHandler.py
@@ -0,0 +1,13 @@
+import os
+
+class FileHandler:
+    def saveImage(self, folder, file) -> None:
+        file.save(os.path.join(folder, file.filename))
+
+    # returns the image path based off the folder and filename. If the requested image does
+    # exist, a default image will be returned
+    def getImagePathOrDefault(self, folder, fileName) -> str:
+        filePath = os.path.join(folder, f"{fileName}.png")
+        fileExists = os.path.isfile(filePath)
+        if (not fileExists): return os.path.join(folder, 'defaultAvatar.png')
+        return filePath
