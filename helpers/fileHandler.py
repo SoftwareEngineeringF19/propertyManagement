739f1bd779d2745be567ef1c0da585608a265927
Domenico Manna <mannadomenico2849@gmail.com>
2019-11-17 21:05:28 -0500

    Add tenant navbar and html template  inheritance

--- helpers/fileHandler.py
+++ helpers/fileHandler.py
@@ -3,11 +3,13 @@ from werkzeug.datastructures import FileStorage
 
 class FileHandler:
     def saveImage(self, folder: str, file: FileStorage) -> None:
+        file.filename = file.filename.lower()
         file.save(os.path.join(folder, file.filename))
 
     # returns the image path based off the folder and filename. If the requested image does
     # exist, a default image will be returned
     def getImagePathOrDefault(self, folder, fileName) -> str:
+        fileName = fileName.lower()
         filePath = os.path.join(folder, f"{fileName}.png")
         fileExists = os.path.isfile(filePath)
         if (not fileExists): return os.path.join(folder, 'defaultAvatar.png')
